f66ac3d88a9ee51beec5b1a750d9cc6a5c0cd579
denisktoo <146120658+denisktoo@users.noreply.github.com>
2025-09-17 16:31:58 +0300

    Revert the last two commits

--- job_board_platform/settings.py
+++ job_board_platform/settings.py
@@ -81,14 +81,8 @@ INSTALLED_APPS = [
 ]
 
 # Celery Configuration
-CELERY_BROKER_URL = env(
-    'CELERY_BROKER_URL',
-    default='amqp://guest:guest@localhost//'  # fallback for local dev
-)
-CELERY_RESULT_BACKEND = env(
-    'CELERY_RESULT_BACKEND',
-    default='rpc://'  # RabbitMQ default result backend
-)
+CELERY_BROKER_URL = 'amqp://guest:guest@localhost//'  # RabbitMQ default
+CELERY_RESULT_BACKEND = 'rpc://'  # Using RPC backend with RabbitMQ
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
@@ -182,8 +176,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
