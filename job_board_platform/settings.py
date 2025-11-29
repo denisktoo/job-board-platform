78155842867552adb07edf074ccc650257309f4d
denisktoo <146120658+denisktoo@users.noreply.github.com>
2025-11-29 18:13:18 +0300

    update: production security setup

--- job_board_platform/settings.py
+++ job_board_platform/settings.py
@@ -69,11 +69,11 @@ DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 # ALLOWED_HOSTS = []
 
-# DEBUG = False
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
